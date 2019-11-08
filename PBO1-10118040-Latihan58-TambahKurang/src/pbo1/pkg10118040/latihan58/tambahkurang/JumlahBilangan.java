c22f60f675247ae5c39e04599d579620ec93f416
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2019-11-08 20:59:43 +0700

    Add files via upload

---
+++ PBO1-10118040-Latihan58-TambahKurang/src/pbo1/pkg10118040/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118040.latihan58.tambahkurang;
+
+/**
+ *
+ * @author asus
+ */
+public class JumlahBilangan extends Bilangan{
+    public void tampilHasilJumlah(){
+        System.out.println("Hasil Penjumlahan = "+(getX()+getY()));
+    }
+}
---
+++ PBO1-10118040-Latihan58-TambahKurang/src/pbo1/pkg10118040/latihan58/tambahkurang/PBO110118040Latihan58TambahKurang.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118040.latihan58.tambahkurang;
+
+/**
+ *
+ * @author asus
+ */
+public class PBO110118040Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+         JumlahBilangan jBilangan = new JumlahBilangan();
+        jBilangan.tampilHasilJumlah();
+        
+        SelisihBilangan sBilangan = new SelisihBilangan();
+        sBilangan.tampilSelisih();
+    }
+    
+}
